c09c41cc484bd053c2f4ef22709bd4d5637188c5
Mahmoud Mohamed Balash <mbh27176@gmail.com>
2022-03-16 21:05:01 +0300

    done

--- script.js
+++ script.js
@@ -7,3 +7,44 @@ let budget = 20
 console.log(budget > 30 &&  budget < 80);
  budget += 30
 console.log(budget >30 && budget < 80);
+//// 
+
+{let grade =prompt ("What is your grade");
+if (grade >= 90 ) {
+    console.log("لقد حصلت على امتياز 🥳");
+}
+else if (grade >= 80 ) {
+    console.log("لقد حصلت على جيد جداً🤩");
+}
+else if (grade >= 70 ) {
+    console.log("لقد حصلت على جيد🙂");
+}
+else if (grade >= 60 ) {
+    console.log("لقد حصلت على مقبول😕");
+}
+else if (grade >= 50  ) {
+    console.log("لقد حصلت على ضعيف☹️");
+}
+else { (grade <= 50) 
+    console.log("راسب💔");
+} }
+//switch statement 
+
+let grade =prompt ("What is your grade")
+
+switch (grade) {
+    case  (grade >=90):
+        console.log( "لقد حصلت على امتياز 🥳" );
+    case (grade >=80) :
+        console.log( "لقد حصلت على جيد جد!" );
+    case (grade >=70):
+        console.log( "لقد حصلت على جي" );
+      case (grade >=60):
+        console.log( "لقد حصلت على مقبول😕" );
+        case (grade >=50):
+            console.log( "لقد حصلت على ضعيف☹️" );
+            case (grade <=50):
+                console.log( "راسب💔" );
+  }
+  
+
