8503cf2337b5b8b5513c6c426eb181fba39accdc
Mahmoud Mohamed Balash <mbh27176@gmail.com>
2022-03-16 19:35:36 +0300

    done

--- script.js
+++ script.js
@@ -2,3 +2,8 @@ alert("mahmoud");
 console.log( 17)
 console.log("%c QNimate is Awesome", "color: green" , "im in 12 grade");
 // java isnt js
+
+let budget = 20
+console.log(budget > 30 &&  budget < 80);
+ budget += 30
+console.log(budget >30 && budget < 80);
