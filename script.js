b86666cc5d7a87ddc8f6856f36c81841d80bfecb
Mahmoud Mohamed Balash <mbh27176@gmail.com>
2022-03-16 18:08:33 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
-
-alert(`mahmoud`.bgblack);
-
+alert("mahmoud");
 console.log( 17)
-console.log("%c QNimate is Awesome", "color: green" `أنا في الصف الحادي عشر`);
-// This is a comment
+console.log("%c QNimate is Awesome", "color: green" , "im in 12 grade");
+// java isnt js
